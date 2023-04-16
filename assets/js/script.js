178405a13837c63ef76cca315ddfe878a9b2104a
vlada_caban <vladaonly@gmail.com>
2023-04-16 19:34:54 -0400

    reorganized content and updated quiz style

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,22 @@
 
 
 const startQuizBtn = document.querySelector(".start-button");
-let questionNumber = 0; //increment when going to next question
+let questionNumber = 0;
 let quizTime = 60;
 const timerCount = document.querySelector(".timer");
 
+//function to count time
 function startTimer() {
   const timerInterval = setInterval(function () {
     quizTime--;
     timerCount.textContent = "Timer: " + quizTime;
 
-    if (quizTime === 0) {
+    if (quizTime <= 0) {
       // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      displayAllDone();
+      //clearInterval(timerInterval);
+      //displayAllDone(); //do i need to display all done if timer is 0?
+      clearTimeout(timerInterval);
+      
     }
   }, 1000);
 }
@@ -59,44 +62,68 @@ const question = [
   },
 ];
 
+// displayCorrect = function () {
+//   let correctAnswer = document.createElement("p");
+//   correctAnswer.innerText = 'Correct!';
+// correctAnswer.parentElement.append(correctAnswer);
+// }
+
 //questions constructor
 let loadQuestion = function () {
-  document.getElementById("questions").innerHTML = "";
+
+  document.getElementById("questions").innerText = "";
   let currentQuestion = question[questionNumber];
   let parentDiv = document.createElement("div");
+  //parentDiv.classList.add("questions-display");
+
   let questionHeader = document.createElement("h2");
-  questionHeader.innerHTML = currentQuestion.question;
+  questionHeader.innerText = currentQuestion.question;
+  
   let btnDiv = document.createElement("div"); //division for all the choices
+  btnDiv.classList.add("answers-display");
+
+  //let correctOrWrong = document.createElement("p");
 
   for (let i = 0; i < currentQuestion.choices.length; i++) {
     let choiceBtn = document.createElement("button");
+    choiceBtn.classList.add("answers-button");
     choiceBtn.innerHTML = currentQuestion.choices[i];
     choiceBtn.addEventListener("click", function (event) {
       event.preventDefault();
-      let selectedChoice = event.target.innerHTML;
+      let selectedChoice = event.target.innerText;
       if (selectedChoice === currentQuestion.answer) {
+        // displayCorrect();
         //display that answers is correct
+        //correctOrWrong.innerText = "Correct!";
+        //go to the next question
         questionNumber++;
         loadQuestion();
       } else {
-        //decrease timer by 10 seconds
-        quizTime -= 10;
+        //decrease timer by 10 seconds if more than 10 seconds left
+        if (quizTime > 10) {
+          quizTime -= 10;
+        } else {
+          quizTime = 1;
+        }
+        // displayWrong();
         //display that answer is incorrect
-        loadQuestion(); //loads the same question again
+        //correctOrWrong.innerText = "Wrong!";
+        //go to the same question again
+        loadQuestion(); 
       }
     });
     btnDiv.append(choiceBtn);
   }
-
   parentDiv.append(questionHeader, btnDiv);
   document.getElementById("questions").append(parentDiv);
+
 };
 
 startQuiz = function (event) {
   event.preventDefault();
 
   //hide welcome screen
-  document.querySelector('.welcome-screen').classList.add('.hide-content');
+  document.querySelector(".welcome-screen").classList.add("hide-content");
   startTimer();
   loadQuestion();
 };
