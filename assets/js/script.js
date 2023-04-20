72fe6b056b366edd3705d9d2f44f1436ca1a696b
vlada_caban <vladaonly@gmail.com>
2023-04-19 22:33:39 -0400

    updated all done section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,17 @@ const startQuizBtn = document.querySelector(".start-button");
 //global variables
 let questionNumber = 0;
 let quizTime = 60;
-const timerCount = document.querySelector(".timer");
+let timerCount = document.querySelector(".timer");
 let answer = document.getElementById("answer");
+let correctAnswersCounter = 0;
+let finalScore = 0;
 
-//function to count time
-function startTimer() {
-  const timerInterval = setInterval(function () {
-    quizTime--;
-    timerCount.textContent = "Timer: " + quizTime;
-    if (quizTime === 0) {
-      clearInterval(timerInterval);
-    }
-  }, 1000);
-}
-
-function displayAllDone() {
-  //create all elements
-  //calculate score based on time left
-}
-
-function showHighscores() {
-  //create all elements for high scores
-  //pull scores from local storage
-}
-
-//questions constructor
-
+//array of questions as objects
 //questions are taken from https://www.w3schools.com/ JavaScript quiz
 const question = [
   {
     question: "Which event occurs when the user clicks on an HTML element?",
-    choices: ["onclick", 
-    "onchange", 
-    "onmouseclick"],
+    choices: ["onclick", "onchange", "onmouseclick"],
     answer: "onclick",
   },
   {
@@ -50,9 +28,7 @@ const question = [
   },
   {
     question: "How to write an IF statement in JavaScript?",
-    choices: ["if i = 5", 
-    "if i = 5 then", 
-    "if (i == 5)"],
+    choices: ["if i = 5", "if i = 5 then", "if (i == 5)"],
     answer: "if (i == 5)",
   },
   {
@@ -75,12 +51,56 @@ const question = [
   },
   {
     question: "How do you round the number 7.25, to the nearest integer?",
-    choices: ["Math.round(7.25)", 
-    "Math.rnd(7.25)", 
-    "round(7.25)"],
+    choices: ["Math.round(7.25)", "Math.rnd(7.25)", "round(7.25)"],
     answer: "Math.round(7.25)",
   },
 ];
+
+let questionsLeft = question.length;
+console.log(questionsLeft);
+
+//function to count time
+function startTimer() {
+  const timerInterval = setInterval(function () {
+    quizTime--;
+    timerCount.textContent = "Timer: " + quizTime;
+    if (quizTime === 0) {
+      clearInterval(timerInterval);
+      // displayAllDone();
+    }
+  }, 1000);
+}
+
+let displayAllDone = function () {
+  finalScore = quizTime;
+
+  //clearing timer
+  quizTime = 1;
+
+  //clearing questions section
+  document.getElementById("questions").innerText = "";
+
+  //rendering all done section
+  let allDoneSection = document.getElementById("all-done");
+  let allDoneTitle = document.createElement("h3");
+
+  let finalScoreParagraph = document.createElement("p");
+
+  if (finalScore < 1) {
+    allDoneTitle.innerText = "All done.";
+    finalScoreParagraph.innerText = `Your final score is ${finalScore}. Better luck next time!`;
+  } else {
+    allDoneTitle.innerText = "Well done!";
+    finalScoreParagraph.innerText = `Your final score is ${finalScore}. Try again to improve!`;
+  }
+
+  allDoneSection.append(allDoneTitle, finalScoreParagraph);
+};
+
+function showHighscores() {
+  //create all elements for high scores
+  //pull scores from local storage
+}
 
 //questions display one by one
 let loadQuestion = function () {
@@ -94,12 +114,12 @@ let loadQuestion = function () {
   let btnDiv = document.createElement("div"); //division for all the choices
   btnDiv.classList.add("answers-display");
 
-  //  let correctOrWrong = document.createElement("p");
-
   for (let i = 0; i < currentQuestion.choices.length; i++) {
     let choiceBtn = document.createElement("button");
     choiceBtn.classList.add("answers-button");
     choiceBtn.innerHTML = currentQuestion.choices[i];
+    btnDiv.append(choiceBtn);
+    document.getElementById("questions").append(questionHeader, btnDiv);
 
     //event listener for answer selection
     choiceBtn.addEventListener("click", function (event) {
@@ -107,8 +127,19 @@ let loadQuestion = function () {
       let selectedChoice = event.target.innerText;
       if (selectedChoice === currentQuestion.answer) {
         answer.innerText = "Correct!";
+        setTimeout(function () {
+          answer.innerText = "";
+        }, 500);
         questionNumber++;
-        loadQuestion();
+        console.log(questionNumber);
+        questionsLeft--;
+        console.log(questionsLeft);
+
+        if (questionsLeft > 0) {
+          loadQuestion();
+        } else {
+          displayAllDone();
+        }
       } else {
         //decrease timer by 10 seconds if more than 10 seconds left
         if (quizTime > 10) {
@@ -117,12 +148,13 @@ let loadQuestion = function () {
           quizTime = 1; //not 0 because it will still go through one loop and get to 0
         }
         answer.innerText = "Wrong!";
+        setTimeout(function () {
+          answer.innerText = "";
+        }, 500);
         loadQuestion();
       }
     });
-    btnDiv.append(choiceBtn);
   }
-  document.getElementById("questions").append(questionHeader, btnDiv);
 };
 
 let startQuiz = function (event) {
